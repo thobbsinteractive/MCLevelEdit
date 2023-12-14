e75172b52408cfea557a43ea198b6128786ffa5c
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-12-14 22:07:59 +0100

    refactor: implemented Infrastructure project

---
+++ MCLevelEdit.Infrastructure/Adapters/FileAdapter.cs
@@ -0,0 +1,166 @@
+﻿using MCLevelEdit.Infrastructure.Interfaces;
+using MCLevelEdit.Model.Domain;
+using MCLevelEdit.Model.Domain.Extensions;
+
+namespace MCLevelEdit.Infrastructure.Adapters;
+
+public class FileAdapter : IFilePort
+{
+    public Task<Map> LoadMap(string fileName)
+    {
+        var levfile = File.ReadAllBytes(fileName);
+        var counter = 0;
+        var Wnumber = 1;
+
+        if (levfile[0] == 82 && levfile[1] == 78 && levfile[2] == 67)
+        {
+            throw new Exception("Compressed level detected! You must uncompress this file first");
+        }
+
+        var manaTarget = levfile[38800];
+        var numWizards = levfile[38802];
+        var manaTotal = BitConverter.ToInt32(levfile, 0);
+
+        GenerationParameters terrainGenerationParameters = new GenerationParameters()
+        {
+            Seed = BitConverter.ToUInt16(levfile, 4),
+            Offset = BitConverter.ToUInt16(levfile, 8),
+            Raise = (ushort)BitConverter.ToUInt32(levfile, 12),
+            Gnarl = BitConverter.ToUInt16(levfile, 16),
+            River = BitConverter.ToUInt16(levfile, 20),
+            Source = (byte)BitConverter.ToUInt16(levfile, 24),
+            SnLin = BitConverter.ToUInt16(levfile, 28),
+            SnFlt = (byte)BitConverter.ToUInt16(levfile, 32),
+            BhLin = (byte)BitConverter.ToUInt16(levfile, 36),
+            BhFlt = (byte)BitConverter.ToUInt16(levfile, 40),
+            RkSte = (byte)BitConverter.ToUInt16(levfile, 44)
+        };
+
+        int fpos = 1090; // Move on to Thing data
+
+        var entityCount = Globals.MAX_ENTITIES;
+
+        var terrain = new Terrain()
+        {
+            GenerationParameters = terrainGenerationParameters
+        };
+
+        Map map = new Map()
+        {
+            Terrain = terrain
+        };
+
+        do
+        {
+            counter++;
+            // Get Thing entries
+            TypeId entityTypeId = (TypeId)BitConverter.ToUInt16(levfile, fpos);
+            //string ThingType = IdentifyThing(classValue);
+            ushort modelId = BitConverter.ToUInt16(levfile, fpos + 2);
+            ushort Xpos = BitConverter.ToUInt16(levfile, fpos + 4);
+            ushort Ypos = BitConverter.ToUInt16(levfile, fpos + 6);
+            ushort DisId = BitConverter.ToUInt16(levfile, fpos + 8);
+            ushort SwiSz = BitConverter.ToUInt16(levfile, fpos + 10);
+            ushort SwiId = BitConverter.ToUInt16(levfile, fpos + 12);
+            ushort Parent = BitConverter.ToUInt16(levfile, fpos + 14);
+            ushort Child = BitConverter.ToUInt16(levfile, fpos + 16);
+
+            string ThingName = "";
+
+            var entityType = EntityTypeExtensions.GetEntityFromTypeIdAndModelId(entityTypeId, modelId);
+
+            if (entityType != null)
+            {
+                map.Entities.Add(new Entity()
+                {
+                    Id = counter,
+                    EntityType = entityType,
+                    Position = new Position(Xpos, Ypos),
+                    Parent = Parent,
+                    Child = Child
+                });
+            }
+
+            //if (classValue == 0)
+            //{
+            //    ThingName = "Blank";
+            //}
+            //else if (classValue == 2)
+            //{
+            //    ThingName = IdentifyScenery(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.Green);
+            //    }
+            //}
+            //else if (classValue == 3)
+            //{
+            //    ThingName = IdentifySpawn(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.Yellow);
+            //    }
+            //}
+            //else if (classValue == 5)
+            //{
+            //    ThingName = IdentifyCreature(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.Red);
+            //    }
+            //}
+            //else if (classValue == 7)
+            //{
+            //    ThingName = IdentifyWeather(Model);
+            //}
+            //else if (classValue == 10)
+            //{
+            //    ThingName = IdentifyEffect(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.Cyan);
+            //    }
+            //}
+            //else if (classValue == 11)
+            //{
+            //    ThingName = IdentifySwitch(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.White);
+            //    }
+            //}
+            //else if (classValue == 12)
+            //{
+            //    ThingName = IdentifySpell(Model);
+            //    if (drawMap == 1)
+            //    {
+            //        bmp.SetPixel(Xpos, Ypos, Color.Purple);
+            //    }
+            //}
+
+            // Define Thing datatable rows
+            //DataRow row = datatable.NewRow();
+
+            //row["thingno"] = ThingNo;
+            //row["class"] = classValue;
+            //row["ThingTypeHidden"] = ThingType;
+            //row["Model"] = Model;
+            //row["ThingNameHidden"] = ThingName;
+            //row["XPos"] = Xpos;
+            //row["YPos"] = Ypos;
+            //row["DisId"] = DisId;
+            //row["swisz"] = SwiSz;
+            //row["SwiId"] = SwiId;
+            //row["parent"] = Parent;
+            //row["child"] = Child;
+            //datatable.Rows.Add(row);
+
+            //ThingCount--;
+
+            fpos += 18;
+            entityCount--;
+        } while (entityCount != 0);
+
+        return Task.FromResult(map);
+    }
+}
---
+++ MCLevelEdit.Infrastructure/Interfaces/IFilePort.cs
@@ -0,0 +1,8 @@
+﻿using MCLevelEdit.Model.Domain;
+
+namespace MCLevelEdit.Infrastructure.Interfaces;
+
+public interface IFilePort
+{
+    Task<Map> LoadMap(string fileName);
+}
