aea919604e58d72d3981a9271a5b917394fc1bba
Timothy Hobbs <thobbsinteractive@gmail.com>
2024-01-28 00:16:38 +0100

    fix: added missing code from last check-in

---
+++ MCLevelEdit/Converter/LayerToBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace MCLevelEdit.Converter;
+
+public class LayerToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString().Equals(parameter);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        switch (parameter)
+        {
+            case "Game":
+                return MCLevelEdit.Model.Enums.Layer.Game;
+                break;
+            case "Height":
+                return MCLevelEdit.Model.Enums.Layer.Height;
+        }
+
+        return BindingOperations.DoNothing;
+    }
+}
